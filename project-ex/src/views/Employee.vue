917bc7bc019562a4433a9aaba06ca39007aad952
Willian Garcia <willian.garcia@softwaresul.com.br>
2023-05-26 09:52:59 -0300

    Criando metodo salvar

--- project-ex/src/views/Employee.vue
+++ project-ex/src/views/Employee.vue
@@ -99,20 +99,20 @@
       
       methods: {
         addEmployee: function(){
-            this.employees.push(this.newEmployee);
-            this.newEmployee = {}
+            // this.employees.push(this.newEmployee);
+            // this.newEmployee = {}
 
-            localStorage.setItem("employees", JSON.stringify(this.employees));
+            // localStorage.setItem("employees", JSON.stringify(this.employees));
         }
         
       },
 
       created() {
-        this.employees = localStorage.getItem("employees") ? JSON.parse(localStorage.getItem("employees")) : this.employees;
+        // this.employees = localStorage.getItem("employees") ? JSON.parse(localStorage.getItem("employees")) : this.employees;
       },
 
       updated() {
-        localStorage.setItem("employees", JSON.stringify(this.employees));
+        // localStorage.setItem("employees", JSON.stringify(this.employees));
       }
       
     } 
